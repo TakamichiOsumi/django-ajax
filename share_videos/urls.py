0387676246d1a0e9f754068f25af14c9a6c247f9
Takamichi Osumi <takamichiosumi01@gmail.com>
2025-06-14 19:27:32 +0900

    Wrote the view to create Hall object.

--- share_videos/urls.py
+++ share_videos/urls.py
@@ -28,6 +28,8 @@ urlpatterns = [
     path('signup/', views.SignUp.as_view(), name = 'signup'),
     path('login/', auth_views.LoginView.as_view(), name = 'login'),
     path('logout/', auth_views.LogoutView.as_view(), name = 'logout'),
+    # Hall
+    path('hall/create', views.create_hall, name = 'create_hall'),
 ]
 
 urlpatterns += static(settings.STATIC_URL,
