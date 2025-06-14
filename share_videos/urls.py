ee327739a6d19ea8da1b810814a0052199d8bf57
Takamichi Osumi <takamichiosumi01@gmail.com>
2025-06-14 18:08:42 +0900

    Added the basic models for Hall and Video.

--- share_videos/urls.py
+++ share_videos/urls.py
@@ -25,9 +25,9 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.home, name = 'home'),
     # AUTH
-    path('signup', views.SignUp.as_view(), name = 'signup'),
-    path('login', auth_views.LoginView.as_view(), name = 'login'),
-    path('logout', auth_views.LogoutView.as_view(), name = 'logout'),
+    path('signup/', views.SignUp.as_view(), name = 'signup'),
+    path('login/', auth_views.LoginView.as_view(), name = 'login'),
+    path('logout/', auth_views.LogoutView.as_view(), name = 'logout'),
 ]
 
 urlpatterns += static(settings.STATIC_URL,
