73c472c7f909da8492b5b382aedd5efc3c4dfe65
Takamichi Osumi <takamichiosumi01@gmail.com>
2025-06-15 14:54:26 +0900

    Enabled CustomUser login.

--- halls/views.py
+++ halls/views.py
@@ -2,14 +2,16 @@ from django.shortcuts import render
 from django.urls import reverse_lazy
 from django.views import generic
 from django.contrib.auth.forms import UserCreationForm
-from .models import Hall
+from .models import CustomUser, Hall
+from .forms import SignupForm
 
 # Create your views here.
 def home(request):
     return render(request, 'halls/home.html')
 
 class SignUp(generic.CreateView):
-    form_class = UserCreationForm
+    model = CustomUser
+    form_class = SignupForm
     success_url = reverse_lazy('home')
     template_name = 'registration/signup.html'
 
