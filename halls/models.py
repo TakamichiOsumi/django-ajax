5f24e0eb2440acb37d83f96a547e87b4432ccdf2
Takamichi Osumi <takamichiosumi01@gmail.com>
2025-06-15 14:10:58 +0900

    Added the missing template file 'create_hall.html' and prepared the Hall models to be created.

--- halls/models.py
+++ halls/models.py
@@ -1,8 +1,20 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
+class CustomUser(AbstractUser):
+    description = models.TextField(verbose_name = 'description',
+                                   null = True,
+                                   blank = True)
+    class Meta:
+        verbose_name_plural = 'CustomUser'
+
 class Hall(models.Model):
     title = models.CharField(max_length = 255)
+    user = models.ForeignKey('halls.CustomUser',
+                             null = True,
+                             blank = True,
+                             on_delete = models.CASCADE)
 
 class Video(models.Model):
     title = models.CharField(max_length = 255)
